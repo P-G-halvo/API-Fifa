97db367b6a1053edb6e747513db0e7168f950bb9
P-G-halvo <pg.halvorsen95@gmail.com>
2025-04-24 14:32:42 +0200

    reverting changes made to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Serve static files from the current folder
 app.use(express.static(__dirname));
